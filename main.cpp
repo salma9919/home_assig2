248d111863ddc80d17a933805d3d90a884d20575
unknown <ghalysalma99@gmail.com>
2023-02-21 19:09:29 +0200

    added average function

--- main.cpp
+++ main.cpp
@@ -13,10 +13,25 @@ int getSum(vector<int>& input)
 
 };
 
+float getAverage(vector<int>& input)
+{
+    int sum=0;
+    float average=0;
+    for(int i=0;i<input.size();i++)
+    {
+        sum+=input[i];
+
+    }
+    average=float(sum)/input.size();
+
+    return average;
+}
+
 int main()
 {
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
 vector<int> A(arr, arr+sizeof(arr)/sizeof(arr[0]));
-    cout<<getSum(A);
+    cout<<getSum(A)<<"\n";
+    cout<<getAverage(A);
     return 0;
 }
