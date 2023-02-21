03de2eb2ab088cea6b7940452030f1ba7d380b0b
unknown <ghalysalma99@gmail.com>
2023-02-21 19:46:07 +0200

    min added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 int getSum(vector<int>& input)
 {
@@ -13,10 +14,31 @@ int getSum(vector<int>& input)
 
 };
 
+float getAverage(vector<int>& input)
+{
+    int sum=0;
+    float average=0;
+    for(int i=0;i<input.size();i++)
+    {
+        sum+=input[i];
+
+    }
+    average=float(sum)/input.size();
+
+    return average;
+};
+int getMin(vector<int>&input)
+{
+  int minvalue=*min_element(input.begin(), input.end()) ;
+    return minvalue;
+};
+
 int main()
 {
-    int arr[] = {1,2,3,4,5};
+    int arr[] = {-1,2,3,-4,5,6,7,8,9,10};
 vector<int> A(arr, arr+sizeof(arr)/sizeof(arr[0]));
-    cout<<getSum(A);
+    cout<<getSum(A)<<"\n";
+    cout<<getAverage(A)<<"\n";
+    cout<<getMin(A);
     return 0;
 }
